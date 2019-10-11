f3f52b8e5b01e0c543a0795b1f9510395b31ef1e
César Gallego Rodríguez <gallego.cesar@gmail.com>
2019-10-11 07:31:27 +0200

    get host

--- internal/server/data/request.go
+++ internal/server/data/request.go
@@ -26,3 +26,20 @@ func getStatus(res http.ResponseWriter, req *http.Request) {
 
 	_, _ = res.Write([]byte(method))
 }
+
+func getHost(res http.ResponseWriter, req *http.Request) {
+	var host string
+	var operation HandlerFunction = func(m *model.Handler) error {
+		host = m.Request.Host
+		return nil
+	}
+
+	vars := mux.Vars(req)
+	hID := vars["handler_id"]
+	err := ReadSafe(hID, operation)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, _ = res.Write([]byte(host))
+}
--- internal/server/data/request_test.go
+++ internal/server/data/request_test.go
@@ -38,6 +38,37 @@ func TestGetMethod(t *testing.T) {
 	responseBytes, _ := ioutil.ReadAll(response.Body)
 	retrieved := string(responseBytes)
 	if retrieved != "GET" {
-		t.Errorf("HTTP Status mismatch. Expected: %s, got: %s", "GET", retrieved)
+		t.Errorf("HTTP Method mismatch. Expected: %s, got: %s", "GET", retrieved)
+	}
+}
+
+func TestGetHost(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "/handlers/HANDLER_XXXXXXXXXX/request/host", nil)
+	response := httptest.NewRecorder()
+	handler := mux.NewRouter()
+	handler.HandleFunc("/handlers/{handler_id}/request/host", getHost).Methods("GET")
+
+	handlerRequest := httptest.NewRequest(http.MethodGet, "/foo/bar", nil)
+	myHandler := &model.Handler{
+		ID:      "HANDLER_XXXXXXXXXX",
+		Request: handlerRequest,
+	}
+
+	ReadSafe = func(id string, f HandlerFunction) error {
+		if id == myHandler.ID {
+			return f(myHandler)
+		}
+		return errors.New("id not found")
+	}
+
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusOK {
+		t.Errorf("HTTP Status mismatch. Expected: %d, got: %d", http.StatusOK, response.Code)
+	}
+
+	responseBytes, _ := ioutil.ReadAll(response.Body)
+	retrieved := string(responseBytes)
+	if retrieved != "example.com" {
+		t.Errorf("Host mistmatch. Expected: %s, got: %s", "example.com", retrieved)
 	}
 }
