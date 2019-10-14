4f7e69a5686d1640f2976c84e5638525f32e2d27
César Gallego Rodríguez <gallego.cesar@gmail.com>
2019-10-14 14:47:47 +0200

    server routes added

--- internal/server/data/server.go
+++ internal/server/data/server.go
@@ -1,8 +1,38 @@
 package data
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 var hasID func(string) bool = Handlers.Has
 
 func Run(address string) error {
-	//TODO: run server
-	return nil
+	r := configRouter()
+
+	return http.ListenAndServe(address, r)
+}
+
+func configRouter() *mux.Router {
+	r := mux.NewRouter()
+	// the request tree
+	r.HandleFunc("/handlers/{handler_id}/request/method", getStatus).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/host", getHost).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/path", getPath).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/matches/{key}", getMatches).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/params/{key}", getParams).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/headers/{key}", getHeader).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/cookies/{key}", getCookies).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/form/{key}", getForm).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/body", getBody).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/files/{file}/filename", getFileName).Methods("GET")
+	r.HandleFunc("/handlers/{handler_id}/request/files/{file}/content", getFileContent).Methods("GET")
+
+	// the response tree
+	r.HandleFunc("/handlers/{handler_id}/response/status", setStatus).Methods("PUT")
+	r.HandleFunc("/handlers/{handler_id}/response/headers/{key}", setHeader).Methods("PUT")
+	r.HandleFunc("/handlers/{handler_id}/response/cookie/{key}", setCookie).Methods("PUT")
+	r.HandleFunc("/handlers/{handler_id}/response/body", setBody).Methods("PUT")
+	return r
 }
